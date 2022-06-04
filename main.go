1de3d2d230e3c604582933c2393b333a6bb2a938
Ilya Kaznacheev <ilya@kaznacheev.me>
2022-06-05 00:18:56 +0300

    Add custom port and log

--- main.go
+++ main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"net"
 	"net/http"
 
 	name "github.com/dustinkirkland/golang-petname"
 )
 
+const port = "80"
+
 var nodeName string
 
 func greet(w http.ResponseWriter, r *http.Request) {
+	log.Printf("request from %s\n", r.RemoteAddr)
 	fmt.Fprintf(w, "Hi, this is %s node", nodeName)
 }
 
 func main() {
+	port := flag.String("p", "8080", "port")
+	flag.Parse()
+
 	name.NonDeterministicMode()
 	nodeName = name.Generate(2, "-")
 
+	log.Printf("starting app on port %s\n", *port)
+	defer log.Println("exiting app")
+
 	http.HandleFunc("/", greet)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(net.JoinHostPort("", *port), nil)
 }
